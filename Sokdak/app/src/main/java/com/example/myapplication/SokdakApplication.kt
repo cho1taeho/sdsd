fd0d935aac4020753c59c12f10474e1bcd8709a2
cho1taeho <cow12dog@gmail.com>
2023-10-23 10:39:33 +0900

    초기설정

--- Sokdak/app/src/main/java/com/example/myapplication/SokdakApplication.kt
+++ Sokdak/app/src/main/java/com/example/myapplication/SokdakApplication.kt
@@ -1,4 +1,20 @@
 package com.example.myapplication
 
-class SokdakApplication {
+import android.app.Application
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+
+import dagger.hilt.android.HiltAndroidApp
+import java.util.concurrent.TimeUnit
+
+@HiltAndroidApp
+class AirbankApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
 }
